2e6dd28cf024b15e8febff9de9be0b8ca1f46ad3
Jeremy Oehrke <Jeremy.Oehrke@hcg.homecredit.net>
2016-04-29 10:09:27 -0500

    split component

--- app/main.ts
+++ app/main.ts
@@ -1,20 +1,34 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {Component} from 'angular2/core';
-import {Pipe, PipeTransform} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
+import {FORM_DIRECTIVES} from 'angular2/common';
+
+@Component({
+    selector: 'iplist',
+    templateUrl: 'partials/iplist.html'
+})
+class IpListComponent{
+    isHidden: boolean = false;
+    @Input() subnetList: Object[];
+
+    constructor(){}
+
+    toggleHidden(){
+        this.isHidden = !this.isHidden
+    }
+}
 
 @Component({
 	selector: 'ipTree',
-	templateUrl: 'partials/hello.html'
+	templateUrl: 'partials/iptree.html',
+    directives: [IpListComponent, FORM_DIRECTIVES]
 })
-
-
-class HelloWorldComponent{
+class IpTreeComponent{
    ipList: Object[] = [];
+    searchedList: Object[] = [];
    ipMap: Object{} = {};
+   searchString: string;
    
-   constructor(){
-      
-   }
+   constructor(){}
    
    fetchIpList(){
       this.ipList = ['1.1.1.1','1.1.1.2','2.1.1.1','3.1.1.1','4.1.1.1'];
@@ -22,6 +36,7 @@ class HelloWorldComponent{
    }
    
    _buildMap(ipList: Object[]){
+        this.ipMap = {};
        for (var ip of ipList){
          var subnet = ip.substring(0,ip.lastIndexOf(".")).concat(".0");
          if (!this.ipMap[subnet]){
@@ -30,19 +45,23 @@ class HelloWorldComponent{
             this.ipMap[subnet].push(ip);
          }
       } 
-      console.log(JSON.stringify(this.ipMap));
+      //console.log(JSON.stringify(this.ipMap));
    }
    
    _generateKeys(obj: any){
       return Object.keys(obj);
    }
-   
-   toggleHide(id: number){
-      console.log("Toggling Hide ".concat(id));
-   }
-   
-   
-   
-   
+
+   _filterList(search: string){
+        this.searchedList = [];
+        this.searchedList = this.ipList.filter(function(item, index, array){
+            return !item.search(search);
+        });
+        this._buildMap(this.searchedList);
+    }
 }
-bootstrap(HelloWorldComponent);
+
+
+
+bootstrap(IpTreeComponent);
+
